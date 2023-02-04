6c2a8c8d8d6e132b3eb4731151c16c7122c873b2
Sordruther <lgbrobowskiii@hotmail.com>
2023-02-04 13:32:45 -0300

    Player Script

---
+++ Assets/Scriptss/PlayerController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravity;
+    public float acceleration = 10;
+    public float distance = 0;
+    public float maxAcceleration = 10;
+    public float maxXVelocity = 100;
+    public float groundHeight = 10;
+    public float jumpVelocity = 20;
+    public float maxHoldJumpTime = 0.4f;
+    public float holdJumpTimer = 0.0f;
+    public float jumpGroundThreshold = 3;
+    
+    public Vector2 velocity;
+
+    public bool isGrounded;
+    public bool isHoldingJump = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        float groundDistance = Mathf.Abs(pos.y - groundHeight);
+
+        if(isGrounded || groundDistance <= jumpGroundThreshold)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                isGrounded = false;
+                velocity.y = jumpVelocity;
+                isHoldingJump = true;
+                holdJumpTimer = 0;
+            }
+
+            
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+            {
+                isHoldingJump = false;
+            }   
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+
+        if(!isGrounded)
+        {
+            if(isHoldingJump)
+            {
+                holdJumpTimer += Time.fixedDeltaTime;
+                if(holdJumpTimer >= maxHoldJumpTime)
+                {
+                    isHoldingJump = false;
+                }
+            }
+
+            pos.y += velocity.y * Time.fixedDeltaTime;
+            if(!isHoldingJump)
+            {
+                velocity.y += gravity * Time.fixedDeltaTime;
+            }
+            
+            
+            if(pos.y <= groundHeight)
+        {
+            pos.y = groundHeight;
+            isGrounded = true;
+        }
+        }
+        
+        distance += velocity.x * Time.fixedDeltaTime;
+
+        if(isGrounded)
+        {
+            
+            float velocityRatio = velocity.x / maxXVelocity;
+            acceleration = maxAcceleration * (1 - velocityRatio);
+
+            velocity.x += acceleration * Time.fixedDeltaTime;
+            if(velocity.x >= maxXVelocity)
+            {
+                velocity.x = maxXVelocity;
+            }
+        }
+        
+
+        transform.position = pos;
+
+
+    }
+}
