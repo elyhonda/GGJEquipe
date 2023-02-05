bddc0c9ed7dd27238a59578a408b4a9f6fe670da
Sordruther <lgbrobowskiii@hotmail.com>
2023-02-05 13:10:09 -0300

    Script Descer Plataforma

---
+++ Assets/Scriptss/PlayerOneWayPlatform.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerOneWayPlatform : MonoBehaviour
+{
+    private GameObject currentOneWayPlatform;
+
+    public BoxCollider2D playerCollider;
+
+    void Start()
+    {
+        playerCollider = GameObject.Find("Player").GetComponent<BoxCollider2D>();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentOneWayPlatform != null)
+            {
+                StartCoroutine(DisableCollision());
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = collision.gameObject;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = null;
+        }
+    }
+
+    private IEnumerator DisableCollision()
+    {
+        BoxCollider2D platformCollider = currentOneWayPlatform.GetComponent<BoxCollider2D>();
+
+        Physics2D.IgnoreCollision(playerCollider, platformCollider);
+        yield return new WaitForSeconds(0.15f);
+        Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
+    }
+}
